82d64c8975d1e81b65716379a19f7a328a9a0e30
Mark <DreynDev@gmail.com>
2025-01-14 15:58:48 +0300

    add basename

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Pizzas/">
     <App />
   </BrowserRouter>,
 );
